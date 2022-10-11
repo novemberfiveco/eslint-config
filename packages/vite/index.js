882bd37d34c408ff1587a30feeea7e07cda21946
Adriaan <adriaan@novemberfive.co>
2022-10-11 16:58:08 +0200

    feature: add eslint config for vite

---
+++ packages/vite/index.js
@@ -0,0 +1,75 @@
+module.exports = {
+  env: { es6: true },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:storybook/recommended",
+    "plugin:prettier/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+  plugins: [
+    "react",
+    "react-hooks",
+    "jest",
+    "import",
+    "prettier",
+    "simple-import-sort",
+    "@typescript-eslint",
+    "jsx-a11y",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  rules: {
+    "jest/no-deprecated-functions": "off",
+    "object-curly-spacing": ["warn", "always"],
+    "import/no-anonymous-default-export": "off",
+    "react/jsx-key": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-empty-interface": [
+      "error",
+      {
+        allowSingleExtends: false,
+      },
+    ],
+    "@typescript-eslint/array-type": ["warn"],
+    "@typescript-eslint/no-non-null-assertion": ["warn"],
+    "no-unused-vars": "off",
+    "no-console": ["warn", { allow: ["warn", "error", "info"] }],
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: true,
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+    "import/newline-after-import": ["error", { count: 1 }],
+    "simple-import-sort/imports": [
+      "warn",
+      {
+        groups: [
+          ["^\\u0000"],
+          ["^@?\\w"],
+          ["^config"],
+          ["^[^.]"],
+          ["^assets"],
+          ["^packages"],
+          ["^utils"],
+          [
+            "^types|^store|^services|^constants|^hooks|^theme|^connectors|^queries",
+          ],
+          ["^pages"],
+          ["^components"],
+          ["^\\."],
+        ],
+      },
+    ],
+    "simple-import-sort/exports": "warn",
+  },
+};
