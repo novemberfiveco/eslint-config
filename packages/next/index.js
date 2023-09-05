9f71eb3f8008427cd0360f13665387a2fffa07c6
ThomasVanleynseeleN5 <thomas.vanleynseele@novemberfive.co>
2023-09-05 21:25:40 +0200

    chore: regroup rules

--- packages/next/index.js
+++ packages/next/index.js
@@ -19,6 +19,8 @@ module.exports = {
     "object-curly-spacing": ["warn", "always"],
     "import/no-anonymous-default-export": "off",
     "react/jsx-key": "error",
+    "no-unused-vars": "off",
+    "no-console": ["warn", { allow: ["warn", "error", "info"] }],
     "@typescript-eslint/no-explicit-any": "error",
     "@typescript-eslint/no-empty-interface": [
       "error",
@@ -28,8 +30,6 @@ module.exports = {
     ],
     "@typescript-eslint/array-type": ["warn"],
     "@typescript-eslint/no-non-null-assertion": ["warn"],
-    "no-unused-vars": "off",
-    "no-console": ["warn", { allow: ["warn", "error", "info"] }],
     "@typescript-eslint/no-unused-vars": [
       "warn",
       {
