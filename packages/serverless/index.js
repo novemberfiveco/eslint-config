0a45f09600ddd2befcf89e2ccc0f5ed6c71ebff5
Min Jie Fan <minjie@novemberfive.co>
2023-09-18 11:21:01 +0200

    feat: initial version

--- packages/serverless/index.js
+++ packages/serverless/index.js
@@ -7,4 +7,62 @@
 
 require("@rushstack/eslint-patch/modern-module-resolution");
 
-module.exports = {};
+module.exports = {
+  env: { es6: true, node: true },
+  parserOptions: {
+    project: true,
+    tsconfigRootDir: __dirname,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended-type-checked",
+    "plugin:prettier/recommended",
+  ],
+  plugins: [
+    "@typescript-eslint",
+    "import",
+    "prettier",
+    "simple-import-sort",
+    "unused-imports",
+  ],
+  rules: {
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        args: "after-used",
+        ignoreRestSiblings: true,
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+      },
+    ],
+    "@typescript-eslint/no-explicit-any": "error",
+    "@typescript-eslint/no-empty-interface": [
+      "error",
+      {
+        allowSingleExtends: false,
+      },
+    ],
+    "@typescript-eslint/no-non-null-assertion": ["warn"],
+    "@typescript-eslint/no-floating-promises": [
+      "error",
+      { ignoreVoid: true, ignoreIIFE: true },
+    ],
+    "import/newline-after-import": ["error", { count: 1 }],
+    "simple-import-sort/imports": ["warn"],
+    "simple-import-sort/exports": "warn",
+
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+    "unused-imports/no-unused-imports": "error",
+  },
+};
