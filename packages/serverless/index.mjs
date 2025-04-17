4d3399764813b5eadc693e5e1e9878ea68162afc
thomas.vanleynseele <thomasmsi1999@gmail.com>
2025-04-17 15:39:36 +0200

    chore: serverless

--- packages/serverless/index.mjs
+++ packages/serverless/index.mjs
@@ -1,40 +1,16 @@
-import pluginNext from "@next/eslint-plugin-next";
-import pluginReactQuery from "@tanstack/eslint-plugin-query";
 import { defineConfig } from "eslint/config";
-import pluginBarrelFiles from "eslint-plugin-barrel-files";
-import pluginReact from "eslint-plugin-react";
-import pluginReactHooks from "eslint-plugin-react-hooks";
-import pluginStorybook from "eslint-plugin-storybook";
+import pluginDrizzle from "eslint-plugin-drizzle";
 
 import typescriptConfig from "@novemberfiveco/eslint-config-typescript";
 
 const config = defineConfig([
   typescriptConfig,
-  pluginReact.configs.flat["recommended"],
-  pluginReact.configs.flat["jsx-runtime"],
-  pluginReactHooks.configs["recommended-latest"],
-  pluginReactQuery.configs["flat/recommended"],
-  pluginStorybook.configs["flat/recommended"],
-  pluginNext.flatConfig["recommended"],
-
   {
     plugins: {
-      "barrel-files": pluginBarrelFiles,
+      drizzle: pluginDrizzle,
     },
     rules: {
-      // We limit the amount of exports in a barrel file, this always causes issues with threeshaking and increased bundle size.
-      // read more here: https://marvinh.dev/blog/speeding-up-javascript-ecosystem-part-7/
-      "barrel-files/avoid-barrel-files": [
-        "error",
-        {
-          amountOfExportsToConsiderModuleAsBarrel: 3,
-        },
-      ],
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
+      ...pluginDrizzle.configs.recommended.rules,
     },
   },
 ]);
