fce0c25ff5862196a568cb88e243c6abddfe082f
thomas.vanleynseele <thomasmsi1999@gmail.com>
2025-04-17 19:56:51 +0200

    chore: remove barrel-files, to constraining for now

--- packages/next/index.mjs
+++ packages/next/index.mjs
@@ -1,7 +1,6 @@
 import pluginNext from "@next/eslint-plugin-next";
 import pluginReactQuery from "@tanstack/eslint-plugin-query";
 import { defineConfig } from "eslint/config";
-import pluginBarrelFiles from "eslint-plugin-barrel-files";
 import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginStorybook from "eslint-plugin-storybook";
@@ -18,22 +17,11 @@ const config = defineConfig([
   pluginNext.flatConfig["recommended"],
 
   {
-    plugins: {
-      "barrel-files": pluginBarrelFiles,
-    },
-
     rules: {
       // We don't want console statements in the codebase
       "no-console": ["warn", { allow: ["warn", "error", "info"] }],
 
-      // We limit the amount of exports in a barrel file, this always causes issues with threeshaking and increased bundle size.
-      // read more here: https://marvinh.dev/blog/speeding-up-javascript-ecosystem-part-7/
-      "barrel-files/avoid-barrel-files": [
-        "error",
-        {
-          amountOfExportsToConsiderModuleAsBarrel: 3,
-        },
-      ],
+      // This rule often results in False Positives.
       "@tanstack/query/exhaustive-deps": "off",
     },
     settings: {
--- packages/vite/index.mjs
+++ packages/vite/index.mjs
@@ -1,6 +1,5 @@
 import pluginReactQuery from "@tanstack/eslint-plugin-query";
 import { defineConfig } from "eslint/config";
-import pluginBarrelFiles from "eslint-plugin-barrel-files";
 import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginStorybook from "eslint-plugin-storybook";
@@ -15,21 +14,11 @@ const config = defineConfig([
   pluginReactQuery.configs["flat/recommended"],
   pluginStorybook.configs["flat/recommended"],
   {
-    plugins: {
-      "barrel-files": pluginBarrelFiles,
-    },
     rules: {
       // We don't want console statements in the codebase
       "no-console": ["warn", { allow: ["warn", "error", "info"] }],
 
-      // We limit the amount of exports in a barrel file, this always causes issues with threeshaking and increased bundle size.
-      // read more here: https://marvinh.dev/blog/speeding-up-javascript-ecosystem-part-7/
-      "barrel-files/avoid-barrel-files": [
-        "error",
-        {
-          amountOfExportsToConsiderModuleAsBarrel: 3,
-        },
-      ],
+      // This rule often results in False Positives.
       "@tanstack/query/exhaustive-deps": "off",
     },
     settings: {
