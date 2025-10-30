4d741d468fd69c52dd4994082436198038e9d2d4
Thomas Vanleynseele <71718555+ThomasVanleynseele@users.noreply.github.com>
2025-10-30 14:06:36 +0100

    chore: use recommended instead of recommended-latest from react hooks (#29)

--- packages/next/index.mjs
+++ packages/next/index.mjs
@@ -12,7 +12,7 @@ const config = defineConfig([
   typescriptConfig,
   pluginReact.configs.flat["recommended"],
   pluginReact.configs.flat["jsx-runtime"],
-  pluginReactHooks.configs.flat["recommended-latest"],
+  pluginReactHooks.configs.flat["recommended"],
   pluginReactQuery.configs["flat/recommended"],
   pluginStorybook.configs["flat/recommended"],
   pluginNext.flatConfig["recommended"],
